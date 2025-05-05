3dffb5e0718b67cd4e9698d3bbb1548b5574a531
clz0421 <33710109+clz0421@users.noreply.github.com>
2025-05-05 23:14:42 +0800

    update

--- main.cpp
+++ main.cpp
@@ -16,17 +16,22 @@ int main(int argc, char *argv[])
     QCoreApplication::setOrganizationDomain("mycompany.com");
     QCoreApplication::setApplicationName("MyApp");
 
-    DiskManager diskManager;
+    DiskManager *diskManager = new DiskManager(&app); 
     QQmlApplicationEngine engine;
     // 把 diskManager 以 “diskManager” 的名字注入到 QML 全局上下文
-    engine.rootContext()->setContextProperty(QStringLiteral("diskManager"), &diskManager);
+    engine.rootContext()->setContextProperty(QStringLiteral("diskManager"), diskManager);
     const QUrl url(QStringLiteral("qrc:/main.qml"));
-    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
-                     &app, [url](QObject *obj, const QUrl &objUrl) {
-        if (!obj && url == objUrl)
-            QCoreApplication::exit(-1);
-    }, Qt::QueuedConnection);
-    engine.load(url);
+
+    if (!engine.load(url)) {
+        qCritical() << "QML load errors:" << engine.errors();
+        return -1;
+    }
+    // QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+    //                  &app, [url](QObject *obj, const QUrl &objUrl) {
+    //     if (!obj && url == objUrl)
+    //         QCoreApplication::exit(-1);
+    // }, Qt::QueuedConnection);
+    // engine.load(url);
 
     if (engine.rootObjects().isEmpty()) {
         qWarning() << "Failed to load QML file.";
